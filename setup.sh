00f81e3f1ba63c92d92e7410aa35c2c44023bf96
Chock Karuppaiah <chock.karuppaiah@eninsys.com>
2017-10-04 08:03:02 -0700

    Created script to set up teh data directory and download files
    This modifies the filesystem and uses curl

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Download the Rotating Disk Electrode data into new directory
+
+# Make the diretory and change to it
+mkdir data2
+cd data2
+
+# Download data
+curl -o rde.csv "https://files.osf.io/v1/resources/qrhcw/providers/osfstorage/59ceb2079ad5a102cd5c41bb?action=download&version=1&direct"
+
